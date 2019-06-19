8ae1935daadbda2be731b126c3e6b25e0d621e6b
kungminlin <mml0816@gmail.com>
2019-06-19 13:39:19 -0700

    fix direction

--- lsm9ds1_input.py
+++ lsm9ds1_input.py
@@ -32,7 +32,7 @@ while True:
 	print("\033[2J")	
 	print('\033[HAcceleration: ({0:0.3f}, {1:0.3f}, {2:0.3f})'.format(accel_x, accel_y, accel_z))
 	print('Magnetometer: ({0:0.3f}, {1:0.3f}, {2:0.3f})'.format(mag_x, mag_y, mag_z))
-	print('Direction: {0:0.3f}'.format())
+	# print('Direction: {0:0.3f}'.format())
 	print('Gyroscope: ({0:0.3f}, {1:0.3f}, {2:0.3f})'.format(gyro_x, gyro_y, gyro_z))
 	print('Temperature: {0:0.3f}C'.format(temp))
 	time.sleep(20)
