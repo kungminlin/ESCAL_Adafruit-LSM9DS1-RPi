6d244b65650946c2718b574bda338f58cf43dc49
kungminlin <mml0816@gmail.com>
2019-06-19 11:00:24 -0700

    fix matplotlib args

--- lsm9ds1_input.py
+++ lsm9ds1_input.py
@@ -15,6 +15,18 @@ y = []
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_lsm9ds1.LSM9DS1_I2C(i2c)
 
+# while True:
+# 	accel_x, accel_y, accel_z = sensor.acceleration
+# 	mag_x, mag_y, mag_z = sensor.magnetic
+# 	gyro_x, gyro_y, gyro_z = sensor.gyro
+# 	temp = sensor.temperature
+
+# 	print("\033[2J")	
+# 	print('\033[HAcceleration: ({0:0.3f}, {1:0.3f}, {2:0.3f})'.format(accel_x, accel_y, accel_z))
+# 	print('Magnetometer: ({0:0.3f}, {1:0.3f}, {2:0.3f})'.format(mag_x, mag_y, mag_z))
+# 	print('Gyroscope: ({0:0.3f}, {1:0.3f}, {2:0.3f})'.format(gyro_x, gyro_y, gyro_z))
+# 	print('Temperature: {0:0.3f}C'.format(temp))
+
 def animate(i, x, y):
 	accel_x, accel_y, accel_z = sensor.acceleration
 	mag_x, mag_y, mag_z = sensor.magnetic
@@ -41,5 +53,5 @@ def animate(i, x, y):
 	print('Gyroscope: ({0:0.3f}, {1:0.3f}, {2:0.3f})'.format(gyro_x, gyro_y, gyro_z))
 	print('Temperature: {0:0.3f}C'.format(temp))
 
-ani = animation.FuncAnimation(fig, animate, farg=(x, y), interval=1000)
+ani = animation.FuncAnimation(fig, animate, fargs=(x, y), interval=1000)
 plt.show()
