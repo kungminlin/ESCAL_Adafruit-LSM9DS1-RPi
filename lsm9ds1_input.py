984867db50a9a4a6b943add8e4677d20c397eec9
kungminlin <mml0816@gmail.com>
2019-06-19 10:43:20 -0700

    add dataplotting

--- lsm9ds1_input.py
+++ lsm9ds1_input.py
@@ -1,43 +1,45 @@
+import datetime
 import time
 import board
 import busio
 import adafruit_lsm9ds1
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import style
 
-import socket
-
-HOST = socket.gethostname('127.0.0.1')
-PORT = 5000
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(5)
+fig = plt.figure()
+axis = fig.add_subplot(1, 1, 1)
+x = []
+y = []
 
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_lsm9ds1.LSM9DS1_I2C(i2c)
 
-while True:
-	connection, address = s.accept()
-	print('Client connection accepted; connected at: ' , address)
-	while True:
-		try:
-			accel_x, accel_y, accel_z = sensor.acceleration
-			mag_x, mag_y, mag_z = sensor.magnetic
-			gyro_x, gyro_y, gyro_z = sensor.gyro
-			temp = sensor.temperature
-			
-			data = str(accel_x)
-			connection.send(data)
-			
-			print("\033[2J")	
-			print('\033[HAcceleration: ({0:0.3f}, {1:0.3f}, {2:0.3f})'.format(accel_x, accel_y, accel_z))
-			print('Magnetometer: ({0:0.3f}, {1:0.3f}, {2:0.3f})'.format(mag_x, mag_y, mag_z))
-			print('Gyroscope: ({0:0.3f}, {1:0.3f}, {2:0.3f})'.format(gyro_x, gyro_y, gyro_z))
-			print('Temperature: {0:0.3f}C'.format(temp))
-			time.sleep(1)
-		except:
-			print('Client connection closed.')
-			break
+def animate(i, x, y):
+	accel_x, accel_y, accel_z = sensor.acceleration
+	mag_x, mag_y, mag_z = sensor.magnetic
+	gyro_x, gyro_y, gyro_z = sensor.gyro
+	temp = sensor.temperature
 
-connection.close()
+	x.append(datetime.datetime.now().strftime('%H:%M:%S:%f'))
+	y.append(mag_x)
 
+	x = x[-20:]
+	y = y[-20:]
 
+	axis.clear()
+	axis.plot(x, y)
+
+	plt.xticks(rotation=45, ha='right')
+	plt.subplots_adjust(bottom=0.30)
+	plt.title('Gyroscope')
+	plt.ylabel('Mag')
+	
+	print("\033[2J")	
+	print('\033[HAcceleration: ({0:0.3f}, {1:0.3f}, {2:0.3f})'.format(accel_x, accel_y, accel_z))
+	print('Magnetometer: ({0:0.3f}, {1:0.3f}, {2:0.3f})'.format(mag_x, mag_y, mag_z))
+	print('Gyroscope: ({0:0.3f}, {1:0.3f}, {2:0.3f})'.format(gyro_x, gyro_y, gyro_z))
+	print('Temperature: {0:0.3f}C'.format(temp))
+
+ani = animation.FuncAnimation(fig, animate, farg=(x, y), interval=1000)
+plt.show()
