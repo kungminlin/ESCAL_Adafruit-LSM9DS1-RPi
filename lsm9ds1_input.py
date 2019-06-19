3075b71d5eebe76092be7cecad30c6ddfe6e315a
kungminlin <mml0816@gmail.com>
2019-06-19 13:24:43 -0700

    fix animation

--- lsm9ds1_input.py
+++ lsm9ds1_input.py
@@ -36,24 +36,24 @@ sensor = adafruit_lsm9ds1.LSM9DS1_I2C(i2c)	# Identify sensor as Adafruit LSM9DS1
 	# print('Gyroscope: ({0:0.3f}, {1:0.3f}, {2:0.3f})'.format(gyro_x, gyro_y, gyro_z))
 	# print('Temperature: {0:0.3f}C'.format(temp))
 
-accel_x, accel_y, accel_z = sensor.acceleration
-mag_x, mag_y, mag_z = sensor.magnetic
-gyro_x, gyro_y, gyro_z = sensor.gyro
-temp = sensor.temperature
+# accel_x, accel_y, accel_z = sensor.acceleration
+# mag_x, mag_y, mag_z = sensor.magnetic
+# gyro_x, gyro_y, gyro_z = sensor.gyro
+# temp = sensor.temperature
 
-dataplot.plot("accel_x", accel_x)
-dataplot.plot("accel_y", accel_y)
-dataplot.plot("accel_z", accel_z)
+# dataplot.plot("accel_x", accel_x)
+# dataplot.plot("accel_y", accel_y)
+# dataplot.plot("accel_z", accel_z)
 
-dataplot.plot("gyro_x", gyro_x)
-dataplot.plot("gyro_y", gyro_y)
-dataplot.plot("gyro_z", gyro_z)
+# dataplot.plot("gyro_x", gyro_x)
+# dataplot.plot("gyro_y", gyro_y)
+# dataplot.plot("gyro_z", gyro_z)
 
-dataplot.plot("mag_x", mag_x)
-dataplot.plot("mag_y", mag_y)
-dataplot.plot("mag_z", mag_z)
+# dataplot.plot("mag_x", mag_x)
+# dataplot.plot("mag_y", mag_y)
+# dataplot.plot("mag_z", mag_z)
 
-dataplot.plot("temp", temp)
+# dataplot.plot("temp", temp)
 
 # Realtime Sensor Data Plotting
 def animate(i):
